94325bc70a9cd18cd016c5c023c5ccab0c9e620a
yk10850 <yk10850@ly.com>
2017-04-11 17:48:54 +0800

    脑抽了

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        //1、1、2、3、5、8、13、21、34
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(9));
+            Console.ReadKey();
+        }
+
+        public static int Fib(int n)
+        {
+            if (n == 2 || n==1)
+            {
+                return 1;
+            }
+            else
+            {
+               return Fib(n - 1)+Fib(n-2);
+            }
+        }
+
+        public static int Sum(int n)
+        {
+            if (n == 2 || n == 1)
+            {
+                return 1;
+            }
+            int sum = 0;
+            int a = 1;
+            int b = 1;
+            for (int i = 2; i < n; i++)
+            {
+                sum = a + b;
+                a = b;
+                b = sum;
+            }
+            return sum;
+        }
+    }
+}
